bd87966a42cffdb16847719e1c96541424bf0bcb
Subash Bose <subash.bose@siemens.com>
2022-06-09 21:19:03 +0530

    resolved the code

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,29 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace AlerterSpace {
-    class Alerter {
+namespace AlerterSpace
+{
+    class Alerter
+    {
         static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
-            Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
+        static int networkAlertStub(float celsius)
+        {
+            Console.WriteLine("ALERT: Temperature is {0} celsius", celsius);
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
+            if (celsius is <= 333 and >= 220)
+            {
+                return 500;
+            }
             return 200;
         }
-        static void alertInCelcius(float farenheit) {
-            float celcius = (farenheit - 32) * 5 / 9;
-            int returnCode = networkAlertStub(celcius);
-            if (returnCode != 200) {
+        static void alertIncelsius(float farenheit)
+        {
+            float celsius = (farenheit - 32) * 5 / 9;
+            int returnCode = networkAlertStub(celsius);
+            if (returnCode != 200)
+            {
                 // non-ok response is not an error! Issues happen in life!
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+                alertFailureCount += 1;
             }
         }
-        static void Main(string[] args) {
-            alertInCelcius(400.5f);
-            alertInCelcius(303.6f);
+        static void Main(string[] args)
+        {
+            alertIncelsius(400.5f);
+            alertIncelsius(303.6f);
+            Debug.Assert(networkAlertStub(230) == 500);
+            Debug.Assert(networkAlertStub(430) == 500);
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Console.WriteLine("All is well (maybe!)\n");
         }
--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-            int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+namespace MisalignedSpace
+{
+    class Misaligned
+    {
+        static int printColorMap(string[] majorColors, string[] minorColors)
+        {
+
+            int i, j = 0;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[j]);
                 }
             }
             return i * j;
         }
-        static void Main(string[] args) {
-            int result = printColorMap();
+        static void Main(string[] args)
+        {
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+            int result = printColorMap(majorColors, minorColors);
             Debug.Assert(result == 25);
             Console.WriteLine("All is well (maybe!)");
         }
